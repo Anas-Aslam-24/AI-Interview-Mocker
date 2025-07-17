d6a44f7b358408ada75a62d2a19b27a644babddc
Mohd Anas <mohdanas0033@gmail.com>
2025-07-18 01:43:49 +0530

    Everything has been resolved!

--- Frontend/src/utils/utils.js
+++ Frontend/src/utils/utils.js
@@ -1,10 +1,3 @@
-// export const USER_API_ENDPOINT = "http://localhost:3000/api/v1/user";
+export const USER_API_ENDPOINT = "http://localhost:3000/api/v1/user";
 
-// export const INTERVIEW_API_ENDPOINT = "http://localhost:3000/api/v1/interview";
-
-
-export const USER_API_ENDPOINT =
-  "https://ai-interview-mocker-j1f1.onrender.com/api/v1/user";
-
-export const INTERVIEW_API_ENDPOINT =
-  "https://ai-interview-mocker-j1f1.onrender.com/api/v1/interview";
+export const INTERVIEW_API_ENDPOINT = "http://localhost:3000/api/v1/interview";
