267e272ea6cd49749703f7e75fe798aeb4403bc4
Mohd Anas <mohdanas0033@gmail.com>
2025-07-18 01:16:17 +0530

    variable change for the deployment

--- Frontend/src/utils/utils.js
+++ Frontend/src/utils/utils.js
@@ -1,3 +1,10 @@
-export const USER_API_ENDPOINT = "http://localhost:3000/api/v1/user";
+// export const USER_API_ENDPOINT = "http://localhost:3000/api/v1/user";
 
-export const INTERVIEW_API_ENDPOINT = "http://localhost:3000/api/v1/interview";
+// export const INTERVIEW_API_ENDPOINT = "http://localhost:3000/api/v1/interview";
+
+
+export const USER_API_ENDPOINT =
+  "https://ai-interview-mocker-j1f1.onrender.com/api/v1/user";
+
+export const INTERVIEW_API_ENDPOINT =
+  "https://ai-interview-mocker-j1f1.onrender.com/api/v1/interview";
