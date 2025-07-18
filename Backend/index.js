63dc1e830f41070e64af5110cd1f10a9884cb571
Mohd Anas <mohdanas0033@gmail.com>
2025-07-18 15:13:46 +0530

    changing port

--- Backend/index.js
+++ Backend/index.js
@@ -34,7 +34,7 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/interview", interviewRoute);
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   connectDB();
