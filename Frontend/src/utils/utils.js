0b1fc8b5fb0c74489da1a578b8fc357365c04329
Mohd Anas <mohdanas0033@gmail.com>
2025-07-27 17:19:23 +0530

    Bug Fix

--- Frontend/src/utils/utils.js
+++ Frontend/src/utils/utils.js
@@ -1,10 +1,10 @@
-export const USER_API_ENDPOINT = "http://localhost:8000/api/v1/user";
+// export const USER_API_ENDPOINT = "http://localhost:8000/api/v1/user";
 
-export const INTERVIEW_API_ENDPOINT = "http://localhost:8000/api/v1/interview";
+// export const INTERVIEW_API_ENDPOINT = "http://localhost:8000/api/v1/interview";
 
 
-// export const USER_API_ENDPOINT =
-//   "https://ai-interview-mocker-4c0e.onrender.com/api/v1/user";
+export const USER_API_ENDPOINT =
+  "https://ai-interview-mocker-4c0e.onrender.com/api/v1/user";
 
-// export const INTERVIEW_API_ENDPOINT =
-//   "https://ai-interview-mocker-4c0e.onrender.com/api/v1/interview";
+export const INTERVIEW_API_ENDPOINT =
+  "https://ai-interview-mocker-4c0e.onrender.com/api/v1/interview";
