2cde0a7518f3b8aa294e40fb943d0400101a53ec
Mohd Anas <mohdanas0033@gmail.com>
2025-07-31 02:27:22 +0530

    updatin key

--- Frontend/src/utils/genAi.js
+++ Frontend/src/utils/genAi.js
@@ -1,6 +1,6 @@
 import { GoogleGenAI } from "@google/genai";
 
-const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY2;
+const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY3;
 
 const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
 
